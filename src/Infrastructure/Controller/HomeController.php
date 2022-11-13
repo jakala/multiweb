26c1a09aa4503eb0033fe849b84c9b243e2b5db8
jakala <jakala@jakala.local>
2022-11-13 02:12:38 +0100

    TICKET-01: el css se gestiona en funcion del host que llama.

--- src/Infrastructure/Controller/HomeController.php
+++ src/Infrastructure/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Infrastructure\Controller;
 
 use App\Domain\ValueObject\GirlRepository;
+use App\Infrastructure\Exception\WebInvalid;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
@@ -10,13 +12,31 @@ class HomeController
 {
     public function __construct(
         readonly GirlRepository $cumlouderGirlRepository,
-        readonly Environment $twig
+        readonly Environment $twig,
+        readonly array $webs
     ) {
     }
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
+        $host = $request->server->get('HTTP_HOST');
+        $web = $this->validate($host);
         $template = '/base.html.twig';
         $list = $this->cumlouderGirlRepository->findAll();
-        return new Response($this->twig->render($template, ['list' => $list->girls]));
+        return new Response($this->twig->render($template, ['list' => $list->girls, 'site' => $web]));
+    }
+
+    private function validate(string $host): array
+    {
+        $found = array_filter(
+            $this->webs,
+            fn($web) => $web['url'] == $host,
+        );
+
+        if(empty($found)) {
+            throw new WebInvalid($host);
+        }
+
+        $keys = array_keys($found);
+        return $found[$keys[0]];
     }
 }
